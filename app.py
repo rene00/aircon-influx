90fa818db32ea6aea0233d8eea89f2110b1b43a7
Rene Cunningham <rene@compounddata.com>
2023-04-08 17:38:43 +1000

    add org

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ async def main(hosts: List[str], **kwargs) -> None:
     influx_token: Union[str, None] = kwargs.get("influx_token", None)
     influx_bucket: Union[str, None] = kwargs.get("influx_bucket", None)
     influx_url: Union[str, None] = kwargs.get("influx_url", None)
+    influx_org: Union[str, None] = kwargs.get("influx_org", None)
 
     udp: UDPServer = UDPServer()
     loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
     udp.run("0.0.0.0", 3610, loop=loop)
     server: ECHONETAPIClient = api(server=udp)
 
-    if influx_url != "" and influx_token != "" and influx_bucket != "": 
-        if all(v is not None for v in (influx_token, influx_bucket, influx_url)):
-            influx_client = InfluxDBClient(url=str(influx_url), token=str(influx_token))
-            write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+    if all(v is not None for v in (influx_token, influx_bucket, influx_url, influx_org)):
+        influx_client = InfluxDBClient(url=str(influx_url), token=str(influx_token))
+        write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
     for host in hosts:
         try:
@@ -49,13 +49,14 @@ async def main(hosts: List[str], **kwargs) -> None:
         if not room_temperature_bytes:
             print(f"Failed to retrieve room temperature for host {host}")
             continue
+
         room_temperature_bytes = bytes(room_temperature_bytes)
         room_temperature: int = int.from_bytes(bytes(room_temperature_bytes), byteorder='big')
         print(f"Room temperature for host {host}: {room_temperature}")
 
         if write_api:
             point: Point = Point("room_temperature").tag("host", host).field("temperature", room_temperature).time(datetime.utcnow())
-            write_api.write(str(influx_bucket), "influxdata", point)
+            write_api.write(str(influx_bucket), influx_org, point)
 
 
 if __name__ == '__main__':
@@ -63,10 +64,12 @@ if __name__ == '__main__':
     influx_url: str = os.environ.get("INFLUX_URL", "")
     influx_token: str = os.environ.get("INFLUX_TOKEN", "")
     influx_bucket: str = os.environ.get("INFLUX_BUCKET", "")
+    influx_org: str = os.environ.get("INFLUX_ORG", "")
     kwargs: Dict[str, str] = {
         "influx_url": influx_url,
         "influx_token": influx_token,
         "influx_bucket": influx_bucket,
+        "influx_org": influx_org,
     }
 
     asyncio.run(main(hosts, kwargs=kwargs))
